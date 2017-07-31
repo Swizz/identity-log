2eb4841fc0f84f16d1e8943f82423c37f55b357f
Swizz <gerodel.quentin@gmail.com>
2017-07-31 18:44:21 +0200

    Add console.* helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-
-export function HelloWorld(name: string): void {
-  console.log("Hello %s", name)
+function tap(fn: (...args: any[]) => void): <T>(...args: T[]) => T
+function tap<T>(fn: (...args: any[]) => void, ...args: T[]): T
+function tap(fn, ...args) {
+  if (args.length > 0) {
+    return fn(...args), args[args.length - 1]
+  }
+  return <T>(...args: T[]): T => (fn(...args), args[args.length - 1])
 }
 
+const assert: <T>(...args: T[]) => T = tap(console.assert)
+const dir: <T>(...args: T[]) => T = tap(console.dir)
+const error: <T>(...args: T[]) => T = tap(console.error)
+const info: <T>(...args: T[]) => T = tap(console.info)
+const log: <T>(...args: T[]) => T = tap(console.log)
+const trace: <T>(...args: T[]) => T = tap(console.trace)
+const warn: <T>(...args: T[]) => T = tap(console.warn)
+
+export { tap, assert, dir, error, info, log, trace, warn }
