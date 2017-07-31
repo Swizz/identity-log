c4ad6098125a9de0939575653294639ff1337c84
Swizz <gerodel.quentin@gmail.com>
2017-07-31 18:47:06 +0200

    Remove irrelevant bench tests

--- bench/index.bench.js
+++ bench/index.bench.js
@@ -1,14 +1,6 @@
 const bench = require('nanobench')
 const { tap } = require('../dist/identity-log.common')
 
-bench('tap implementation 1 arg 1 time', b => {
-  const fn = arg1 => {}
-
-  b.start()
-  tap(fn, 1)
-  b.end()
-})
-
 bench('tap implementation 1 arg 100.000 times', b => {
   const fn = arg1 => {}
 
@@ -36,14 +28,6 @@ bench('tap implementation 10 args 100.000 times', b => {
   for (let i = 0; i < 100000; i++) {
     tap(fn, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   }
-  b.end()
-})
-
-bench('curry tap implementation 1 arg 1 time', b => {
-  const fn = arg1 => {}
-
-  b.start()
-  tap(fn)(1)
   b.end()
 })
 
